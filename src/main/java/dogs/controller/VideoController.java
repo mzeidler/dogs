72da12f019ac96e58374b6684e37425983fb6b57
mzeidler <mzeidler3@gmail.com>
2019-07-07 21:01:34 +0200

    Adding Story Videos

--- src/main/java/dogs/controller/VideoController.java
+++ src/main/java/dogs/controller/VideoController.java
@@ -2,9 +2,11 @@ package dogs.controller;
 
 import dogs.model.Dog;
 import dogs.model.Image;
+import dogs.model.Story;
 import dogs.model.Video;
 import dogs.service.DogService;
 import dogs.service.ImageService;
+import dogs.service.StoryService;
 import dogs.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -28,6 +30,9 @@ public class VideoController {
     @Autowired
     private DogService dogService;
 
+    @Autowired
+    private StoryService storyService;
+
     @GetMapping("/api/video/{id}")
     public Video findById(@PathVariable Long id) {
         return videoService.get(id);
@@ -46,12 +51,23 @@ public class VideoController {
         videoService.deleteVideo(id);
     }
 
-    @PostMapping("/api/video/{dogid}")
-    public Video saveVideo(@PathVariable Long dogid, @RequestBody Video video) {
+    @PostMapping("/api/video/dog/{dogid}")
+    public Video saveDogVideo(@PathVariable Long dogid, @RequestBody Video video) {
 
         Dog dog = dogService.getDog(dogid);
         if (dog != null) {
             video.setDog(dog);
+        }
+
+        return videoService.save(video);
+    }
+
+    @PostMapping("/api/video/story/{storyid}")
+    public Video saveStoryVideo(@PathVariable Long storyid, @RequestBody Video video) {
+
+        Story story = storyService.getStory(storyid);
+        if (story != null) {
+            video.setStory(story);
         }
 
         return videoService.save(video);
